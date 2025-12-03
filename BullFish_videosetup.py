65f4bde4603d730bb1816b8bf3744e501062a8e5
CH2BrCH2N3 <120793082+CH2BrCH2N3@users.noreply.github.com>
2025-12-03 23:27:05 +0800

    Refactor video setup and metadata handling
    Updated video setup process to streamline metadata collection and added parameters for video rotation and cropping.

--- BullFish_videosetup.py
+++ BullFish_videosetup.py
@@ -1,26 +1,14 @@
 from pathlib import Path
-from copy import copy, deepcopy
+from copy import deepcopy
 from traceback import print_exc
 import cv2 as cv
-from BullFish_pkg.general import getfilepath, create_path, get_input, load_settings
+from BullFish_pkg.general import getfilepath, create_path, get_input
 from BullFish_pkg.cv_editing import supported_formats, get_rm, frame_rotate
 import pandas as pd
 
-default_settings = {
-    "video_start": -1,
-    "video_end": -1,
-    "rotate": 181,
-    "crop_tlx": -1,
-    "crop_tly": -1,
-    "crop_x": -1,
-    "crop_y": -1,
-    "swimarea_tlx": -1,
-    "swimarea_tly": -1,
-    "swimarea_x": -1,
-    "swimarea_y": -1,
-    "naming": 0,
+settings = {
+    "naming": 1,
     "check": 1}
-settings = load_settings('videosetup', default_settings)
 
 metadata_all = []
 metadata_path = Path.cwd() / 'metadata.csv'
@@ -62,87 +50,84 @@ while True:
             'fps': fps,
             'x_original': x_original,
             'y_original': y_original}
-        metadata.update(copy(settings))
-        del metadata['naming']
-        del metadata['check']
         
         print('Now, choose the video segment for tracking.')
         print('Be aware of the frame rate as you need to enter the frame numbers instead of the time.')
-        if settings['video_start'] == -1:
-            metadata['video_start'] = get_input(int, 'What is the starting frame?')
-        if settings['video_end'] == -1:
-            metadata['video_end'] = get_input(int, 'What is the ending frame?')
+        video_start = get_input(int, 'What is the starting frame?')
+        video_end = get_input(int, 'What is the ending frame?')
         while True:
-            if metadata['video_end'] < framenumber_original and metadata['video_start'] < metadata['video_end']:
+            if video_end < framenumber_original and video_start < video_end:
                 break
             else:
                 print('Unacceptable range. Choose again.')
-                metadata['video_start'] = get_input(int, 'What is the starting frame?')
-                metadata['video_end'] = get_input(int, 'What is the ending frame?')
+                video_start = get_input(int, 'What is the starting frame?')
+                video_end = get_input(int, 'What is the ending frame?')
         
-        video.set(cv.CAP_PROP_POS_FRAMES, metadata['video_start'])
+        video.set(cv.CAP_PROP_POS_FRAMES, video_start)
         ret, frame1 = video.read()
         cv.imwrite(f'{p.stem}_Frame1.png', frame1)
         video.release()
         print('The first frame of the selected video segment has been saved at the current directory.')
         print('The following part is to set the parameters for rotating and cropping the video to prepare for tracking.')
         
-        if settings['rotate'] == 181:
-            metadata['rotate'] = get_input(float, 'Enter the video rotation angle (clockwise, enter within -179.9 to 180):')
-        if settings['crop_tlx'] == -1:
-            metadata['crop_tlx'] = get_input(int, 'Enter the x-coordinate of the top-left corner of the cropping area:')
-        if settings['crop_tly'] == -1:
-            metadata['crop_tly'] = get_input(int, 'Enter the y-coordinate of the top-left corner of the cropping area:')
-        if settings['crop_x'] == -1:
-            metadata['crop_x'] = get_input(int, 'Enter the width (x) of the cropping area:')
-        if settings['crop_y'] == -1:
-            metadata['crop_y'] = get_input(int, 'Enter the height (y) of the cropping area:')
+        rotate = get_input(float, 'Enter the video rotation angle (clockwise, enter within -179.9 to 180):')
+        crop_tlx = get_input(int, 'Enter the x-coordinate of the top-left corner of the cropping area:')
+        crop_tly = get_input(int, 'Enter the y-coordinate of the top-left corner of the cropping area:')
+        crop_x = get_input(int, 'Enter the width (x) of the cropping area:')
+        crop_y = get_input(int, 'Enter the height (y) of the cropping area:')
         
         while settings['check']:
             try:
-                rm = get_rm(x_original, y_original, metadata['rotate'])
-                frame_edited = frame_rotate(frame1, x_original, y_original, metadata['rotate'], rm)
-                crop_brx = metadata['crop_tlx'] + metadata['crop_x']
-                crop_bry = metadata['crop_tly'] + metadata['crop_y']
-                if metadata['crop_x'] != 0 and metadata['crop_y'] != 0:
-                    frame_edited = frame_edited[metadata['crop_tly']:crop_bry, metadata['crop_tlx']:crop_brx]
+                rm = get_rm(x_original, y_original, rotate)
+                frame_edited = frame_rotate(frame1, x_original, y_original, rotate, rm)
+                crop_brx = crop_tlx + crop_x
+                crop_bry = crop_tly + crop_y
+                if crop_x != 0 and crop_y != 0:
+                    frame_edited = frame_edited[crop_tly:crop_bry, crop_tlx:crop_brx]
                 cv.imwrite(f'{p.stem}_edited_frame.png', frame_edited)
                 if input('The rotated and cropped frame is saved. Enter f to carry on, others to change:') == 'f':
                     break
             except Exception:
                 print('An error occurred when editing the frame. Change parameters.')
                 print_exc()
-            metadata['rotate'] = get_input(float, 'Enter the video rotation angle (clockwise, enter within -179.9 to 180):')
-            metadata['crop_tlx'] = get_input(int, 'Enter the x-coordinate of the top-left corner of the cropping area:')
-            metadata['crop_tly'] = get_input(int, 'Enter the y-coordinate of the top-left corner of the cropping area:')
-            metadata['crop_x'] = get_input(int, 'Enter the width (x) of the cropping area:')
-            metadata['crop_y'] = get_input(int, 'Enter the height (y) of the cropping area:')
+            rotate = get_input(float, 'Enter the video rotation angle (clockwise, enter within -179.9 to 180):')
+            crop_tlx = get_input(int, 'Enter the x-coordinate of the top-left corner of the cropping area:')
+            crop_tly = get_input(int, 'Enter the y-coordinate of the top-left corner of the cropping area:')
+            crop_x = get_input(int, 'Enter the width (x) of the cropping area:')
+            crop_y = get_input(int, 'Enter the height (y) of the cropping area:')
         
         size = frame_edited.shape
         x_current = size[1]
         y_current = size[0]
         
-        if settings['swimarea_tlx'] == -1:
-            metadata['swimarea_tlx'] = get_input(int, 'Enter the x-coordinate of the top-left corner of the swimming area:')
-        if settings['swimarea_tly'] == -1:
-            metadata['swimarea_tly'] = get_input(int, 'Enter the y-coordinate of the top-left corner of the swimming area:')
-        if settings['swimarea_x'] == -1:
-            metadata['swimarea_x'] = get_input(int, 'Enter the width (x) of the swimming area:')
-        if settings['swimarea_y'] == -1:
-            metadata['swimarea_y'] = get_input(int, 'Enter the height (y) of the swimming area:')
+        swimarea_tlx = get_input(int, 'Enter the x-coordinate of the top-left corner of the swimming area:')
+        swimarea_tly = get_input(int, 'Enter the y-coordinate of the top-left corner of the swimming area:')
+        swimarea_x = get_input(int, 'Enter the width (x) of the swimming area:')
+        swimarea_y = get_input(int, 'Enter the height (y) of the swimming area:')
         
         while settings['check']:
             frame_l = deepcopy(frame_edited)
-            cv.rectangle(frame_l, (metadata['swimarea_tlx'], metadata['swimarea_tly']), (metadata['swimarea_tlx'] + metadata['swimarea_x'], metadata['swimarea_tly'] + metadata['swimarea_y']), (0, 0, 255), 3)
+            cv.rectangle(frame_l, (swimarea_tlx, swimarea_tly), (swimarea_tlx + swimarea_x, swimarea_tly + swimarea_y), (0, 0, 255), 3)
             cv.imwrite(f'{p.stem}_labeled_frame.png', frame_l)
             if input('The swimming area is labeled in a new figure. Enter f to carry on, others to change:') == 'f':
                 break
-            metadata['swimarea_tlx'] = get_input(int, 'Enter the x-coordinate of the top-left corner of the swimming area:')
-            metadata['swimarea_tly'] = get_input(int, 'Enter the y-coordinate of the top-left corner of the swimming area:')
-            metadata['swimarea_x'] = get_input(int, 'Enter the width (x) of the swimming area:')
-            metadata['swimarea_y'] = get_input(int, 'Enter the height (y) of the swimming area:')
+            swimarea_tlx = get_input(int, 'Enter the x-coordinate of the top-left corner of the swimming area:')
+            swimarea_tly = get_input(int, 'Enter the y-coordinate of the top-left corner of the swimming area:')
+            swimarea_x = get_input(int, 'Enter the width (x) of the swimming area:')
+            swimarea_y = get_input(int, 'Enter the height (y) of the swimming area:')
         
         metadata.update({
+            'video_start': video_start,
+            'video_end': video_end,
+            'rotate': rotate,
+            'crop_tlx': crop_tlx,
+            'crop_tly': crop_tly,
+            'crop_x': crop_x,
+            'crop_y': crop_y,
+            'swimarea_tlx': swimarea_tlx,
+            'swimarea_tly': swimarea_tly,
+            'swimarea_x': swimarea_x,
+            'swimarea_y': swimarea_y,
             'x_current': x_current,
             'y_current': y_current})
         metadata_all.append(metadata)
